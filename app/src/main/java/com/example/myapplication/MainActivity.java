084e884f29dddcebc31b1b7d9ccec056995ce505
ORAM RAHAMIM MUZIKANT <oram1994@gmail.com>
2022-10-18 01:07:01 +0300

    new changes 22

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -25,7 +25,7 @@ import java.util.Map;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-//oram-changes- 2
+//oram-changes- 3
     MaterialToolbar materialToolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
