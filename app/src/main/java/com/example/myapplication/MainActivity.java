804288ec2964084e04f419b22318befb31925033
ORAM RAHAMIM MUZIKANT <oram1994@gmail.com>
2022-10-18 01:16:21 +0300

    New Branch dsfdf

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -25,7 +25,7 @@ import java.util.Map;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-//oram-changes- new
+//oram-changes- new try
     MaterialToolbar materialToolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
